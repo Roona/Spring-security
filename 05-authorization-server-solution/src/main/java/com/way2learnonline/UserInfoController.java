3486f0fe9d51572c9c6d9ddcf081fe0580ac0c84
Roona <roona.sa@gmail.com>
2022-01-10 16:14:35 -0600

    Add files via upload

---
+++ 05-authorization-server-solution/src/main/java/com/way2learnonline/UserInfoController.java
@@ -0,0 +1,24 @@
+package com.way2learnonline;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserInfoController {
+	
+	 @GetMapping("/validateUser")
+	    public Principal user(Principal principal) {
+	        return principal;
+	    }
+	
+	
+
+}
